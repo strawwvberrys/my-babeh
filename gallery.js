a5045c262fb378356c6da31f2bba9380e5ef820a
pinknprettyy <sienaaskulls@gmail.com>
2026-07-27 18:53:55 +1200

    e

--- gallery.js
+++ gallery.js
@@ -170,23 +170,12 @@ const pages = [
 
 {
     leftPhoto: "images/photo43.png",
-    leftText: "The end... but our story hasn't ended",
+    leftText: "♡ The end... but our story hasn't ended♡",
 
     rightPhoto: "images/photo1.png",
     rightText: "I love you more than words can ever say."
 }
-document.querySelectorAll(".scrap").forEach(card=>{
-    card.style.opacity="0";
-    card.style.transform+=" scale(.96)";
-});
 
-setTimeout(()=>{
-    showPhotos();
-
-    document.querySelectorAll(".scrap").forEach(card=>{
-        card.style.opacity="1";
-    });
-},200);
 ];
 
 let current = 0;
@@ -223,62 +212,3 @@ function previousPage(){
 }
 
 showPage();
-
-window.addEventListener("load",()=>{
-
-    setTimeout(()=>{
-
-        const loader=document.getElementById("loader");
-
-        loader.style.opacity="0";
-
-        setTimeout(()=>{
-
-            loader.style.display="none";
-
-        },800);
-
-    },1700);
-
-});
-document.addEventListener("mousemove",function(e){
-
-    const sparkle=document.createElement("div");
-
-    sparkle.className="sparkle";
-
-    sparkle.innerHTML="✦";
-
-    sparkle.style.left=e.pageX+"px";
-
-    sparkle.style.top=e.pageY+"px";
-
-    document.body.appendChild(sparkle);
-
-    setTimeout(()=>{
-
-        sparkle.remove();
-
-    },800);
-
-});document.addEventListener("mousemove",function(e){
-
-    const sparkle=document.createElement("div");
-
-    sparkle.className="sparkle";
-
-    sparkle.innerHTML="✦";
-
-    sparkle.style.left=e.pageX+"px";
-
-    sparkle.style.top=e.pageY+"px";
-
-    document.body.appendChild(sparkle);
-
-    setTimeout(()=>{
-
-        sparkle.remove();
-
-    },800);
-
-});
